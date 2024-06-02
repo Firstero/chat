1ba4d278d30743374cebb676436cd8e56609d12a
Firstero <firstbear0@protonmail.com>
2024-06-02 22:41:41 +0800

    Feature: refractor code to a workspace and start notify server

--- notify_server/src/lib.rs
+++ notify_server/src/lib.rs
@@ -2,9 +2,11 @@ mod sse;
 
 use axum::{routing::get, Router};
 
-use sse::sse_handler;
+use sse::{index_handler, sse_handler};
 
 pub fn get_router() -> Router {
     // build our application with a route
-    Router::new().route("/events", get(sse_handler))
+    Router::new()
+        .route("/", get(index_handler))
+        .route("/events", get(sse_handler))
 }
--- notify_server/src/sse.rs
+++ notify_server/src/sse.rs
@@ -1,21 +1,27 @@
 use std::{convert::Infallible, time::Duration};
 
-use axum::response::sse::{Event, Sse};
+use axum::response::{
+    sse::{Event, Sse},
+    Html, IntoResponse,
+};
 
 use axum_extra::{headers, TypedHeader};
 use futures::{self, stream, Stream};
 use tokio_stream::StreamExt as _;
+use tracing::info;
+
+const INDEX_HTML: &str = include_str!("../index.html");
 
 pub(crate) async fn sse_handler(
     TypedHeader(user_agent): TypedHeader<headers::UserAgent>,
 ) -> Sse<impl Stream<Item = Result<Event, Infallible>>> {
-    println!("`{}` connected", user_agent.as_str());
+    info!("`{}` connected", user_agent.as_str());
 
     // A `Stream` that repeats an event every second
     //
     // You can also create streams from tokio channels using the wrappers in
     // https://docs.rs/tokio-stream
-    let stream = stream::repeat_with(|| Event::default().data("hi!"))
+    let stream = stream::repeat_with(|| Event::default().data("hello, sse client!"))
         .map(Ok)
         .throttle(Duration::from_secs(1));
 
@@ -24,4 +30,8 @@ pub(crate) async fn sse_handler(
             .interval(Duration::from_secs(1))
             .text("keep-alive-text"),
     )
+}
+
+pub(crate) async fn index_handler() -> impl IntoResponse {
+    Html(INDEX_HTML)
 }

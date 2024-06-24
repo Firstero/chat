02ecc51525f2c54bf6cad6122c73a25e84c3629f
Firstero <firstbear0@protonmail.com>
2024-06-24 22:47:43 +0800

    feature: add auth middleware unit test

--- chat_server/src/middlewares/auth.rs
+++ chat_server/src/middlewares/auth.rs
@@ -36,3 +36,57 @@ pub async fn verify_token(State(state): State<AppState>, req: Request, next: Nex
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::{AppConfig, User};
+
+    use super::*;
+    use anyhow::Result;
+    use axum::{body::Body, http::Request, middleware::from_fn_with_state, routing::get, Router};
+    use tower::ServiceExt;
+
+    async fn handler() -> impl IntoResponse {
+        (StatusCode::OK, "ok")
+    }
+
+    #[tokio::test]
+    async fn verify_token_should_work() -> Result<()> {
+        let config = AppConfig::try_load()?;
+
+        let user = User::new(1, "test", "password");
+        let (_tdb, state) = AppState::new_for_test(config).await?;
+        let token = state.sk.encode(user)?;
+        let app = Router::new()
+            .route("/", get(handler))
+            .layer(from_fn_with_state(state.clone(), verify_token));
+
+        // good token
+        let req = Request::builder()
+            .uri("/")
+            .header("Authorization", format!("Bearer {}", token))
+            .body(Body::empty())?;
+        let res = app.clone().oneshot(req).await?;
+        assert_eq!(res.status(), StatusCode::OK);
+
+        // no token
+        let res = app
+            .clone()
+            .oneshot(Request::builder().uri("/").body(Body::empty())?)
+            .await?;
+        assert_eq!(res.status(), StatusCode::UNAUTHORIZED);
+
+        // bad token
+        let res = app
+            .clone()
+            .oneshot(
+                Request::builder()
+                    .uri("/")
+                    .header("Authorization", "Bearer bad token")
+                    .body(Body::empty())?,
+            )
+            .await?;
+        assert_eq!(res.status(), StatusCode::FORBIDDEN);
+        Ok(())
+    }
+}
